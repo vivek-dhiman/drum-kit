c460067792d261e381f1530bc97e3c6aac5b9b0e
Vivek <vivekdhiman1208@gmail.com>
2022-09-04 07:40:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,35 +14,35 @@ document.addEventListener("keydown",function(event){
 function makeSound(key){
     switch (key) {
         case "w":
-          var a1 = new Audio("sounds/tom-1.mp3");
+          var a1 = new Audio("tom-1.mp3");
           a1.play();
           break;
         case "a":
-          var a2 = new Audio("sounds/tom-2.mp3");
+          var a2 = new Audio("tom-2.mp3");
           a2.play();
           break;
         case "s":
-          var a3 = new Audio("sounds/tom-3.mp3");
+          var a3 = new Audio("tom-3.mp3");
           a3.play();
           break;
         case "d":
-          var a4 = new Audio("sounds/tom-4.mp3");
+          var a4 = new Audio("tom-4.mp3");
           a4.play();
           break;
         case "j":
-          var a5 = new Audio("sounds/snare.mp3");
+          var a5 = new Audio("snare.mp3");
           a5.play();
           break;
         case "k":
-          var a6 = new Audio("sounds/crash.mp3");
+          var a6 = new Audio("crash.mp3");
           a6.play();
           break;
         case "l":
-          var a7 = new Audio("sounds/kick-bass.mp3");
+          var a7 = new Audio("kick-bass.mp3");
           a7.play();
           break;
         default:
-          var a6 = new Audio("sounds/crash.mp3");
+          var a6 = new Audio("crash.mp3");
           a6.play();
       }
     }
